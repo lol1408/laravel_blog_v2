8cf1e9e14aba21192258159729e8b431e6b85286
Dante de Braso <sergey.king96@gmail.com>
2018-04-16 07:36:52 +0300

    added layouts and content for dashboard page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,43 @@
-@extends('layouts.app')
+@extends('admin.layouts.app_admin')
 @section('content')
-    <h1>Hello it's me</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Категории 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Материалов 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Посетители 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Сегодня 0</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a href="#" class="btn btn-block btn-default">Создание категории</a>
+                <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Категории первая</h4>
+                    <p class="list-group-item-text">Кол-во материалов</p>
+                </a>
+            </div>
+            <div class="col-sm-6">
+                <a href="#" class="btn btn-block btn-default">Создание материала</a>
+                <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Материал первый</h4>
+                    <p class="list-group-item-text">Категория</p>
+                </a>
+            </div>
+        </div>
+    </div>
 @endsection
